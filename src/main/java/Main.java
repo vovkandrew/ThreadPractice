1bd9d13ed67648a08202e20c68792291d9869d55
VovkAA <vovkandrew@gmail.com>
2020-07-03 17:08:38 +0300

    New method to calculate sum created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,31 +17,31 @@ public class Main {
         for (int i = 0; i < SIZE; i++) {
             digits.add(new Random().nextInt(100));
         }
+        int totalSum = calculateSum(es, digits);
+        es.shutdown();
+        ForkJoinPool pool = new ForkJoinPool(NUM_OF_THREADS);
+        int totalSumFork =
+                pool.invoke(new SummingSingleThreadToolPartTwo(digits, NUM_OF_THREADS));
+        System.out.println(totalSum == totalSumFork);
+    }
+
+    private static Integer calculateSum(ExecutorService es, List<Integer> digits) {
         int start = 0;
         int step = SIZE / NUM_OF_THREADS;
         int end = SIZE / NUM_OF_THREADS;
         int totalSum = 0;
-        System.out.println(LocalTime.now());
         for (int i = 0; i < NUM_OF_THREADS; i++) {
             try {
                 totalSum = totalSum
                         + (Integer) es.submit(
-                                new SummingSingleThreadTool(digits.subList(start, end)))
-                                .get();
+                        new SummingSingleThreadTool(digits.subList(start, end)))
+                        .get();
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
             start = start + step;
             end = end + step;
         }
-        System.out.println(LocalTime.now());
-        es.shutdown();
-        System.out.println(totalSum);
-        ForkJoinPool pool = new ForkJoinPool(NUM_OF_THREADS);
-        System.out.println(LocalTime.now());
-        int totalSumFork =
-                pool.invoke(new SummingSingleThreadToolPartTwo(digits, NUM_OF_THREADS));
-        System.out.println(LocalTime.now());
-        System.out.println(totalSumFork);
+        return totalSum;
     }
 }
